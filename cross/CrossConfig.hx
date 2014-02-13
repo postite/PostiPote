fa9ec36a2350b16a594e878638647e1c8d1cc622
postite <blog@postite.com>
2014-02-13 15:40:11 +0100

    remove credentials

--- cross/CrossConfig.hx
+++ cross/CrossConfig.hx
@@ -6,7 +6,7 @@ class CrossConfig
 	#if local
 	static var params={ user : "root", port :8889, pass : "root", host : "localhost", database :"postite" }
 	#else
-	static var params={ user : "postite", port :3306, pass : "paglop", host : "mysql1.alwaysdata.com", database :"postite_it" }
+	static var params={ user : "postite", port :3306, pass : "*****", host : "mysql1.alwaysdata.com", database :"postite_it" }
 	#end
 	static var uploadPath="uploads";
 
