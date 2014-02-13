c4ccf9ba685c72f1ffc3525b34b543b7c9a09b46
postite <blog@postite.com>
2014-02-13 15:38:52 +0100

    remeove credentials

--- server/Server.hx
+++ server/Server.hx
@@ -9,12 +9,6 @@ class Server
 {
 
 
-	// #if local
-	// static var params={ user : "root", port :8889, pass : "root", host : "localhost", database :"postite" }
-	// #else
-	// static var params={ user : "postite", port :3306, pass : "paglop", host : "mysql1.alwaysdata.com", database :"postite_it" }
-	// #end
-	// public static var uploadPath="uploads";
 
 	function new()
 	{
